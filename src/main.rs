9123203f82c2c24ab77371981c6a24d082a8773e
Michael Gebauer <gebauerm23@gmail.com>
2026-04-06 01:45:39 +0200

    feat: Add Facing component, player sprites and transform projection system
    * Introduce Facing rotation component with required Transform dependency
    * Add project_positions system to sync Position/Facing values to entity transforms
    * Load player sprite asset, spawn both players with correctly oriented facing rotation
    * Register position projection system to run on FixedUpdate game loop
    * Add stub sprite movement system for upcoming movement implementation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use bevy::math::bounding::{Aabb2d, BoundingVolume, IntersectsVolume};
 use bevy::prelude::*;
+use std::f32::consts::FRAC_PI_2;
 
 #[derive(Component, Default)]
+#[require(Transform)]
 struct Position(Vec2);
+
+#[derive(Component, Default)]
+#[require(Transform)]
+struct Facing(Quat);
 
 #[derive(Component)]
 #[require(Position)]
@@ -12,25 +18,52 @@ struct Attacker;
 #[require(Position)]
 struct Defender;
 
-fn spawn_players(mut commands: Commands, window: Single<&Window>) {
+fn project_positions(mut positionables: Query<(&mut Transform, &Position, &Facing)>) {
+    for (mut transform, position, facing) in &mut positionables {
+        // Extend is going to turn this from a Vec2 to a Vec3
+        transform.translation = position.0.extend(0.);
+        transform.rotation = facing.0;
+    }
+}
+fn spawn_players(mut commands: Commands, window: Single<&Window>, asset_server: Res<AssetServer>) {
     let half_window_size = window.resolution.size() / 2.;
     let padding = 20.;
 
-    let attacker_pos = Vec2::new(half_window_size.x - padding, 0.);
-    commands.spawn((Attacker, Position(attacker_pos)));
+    let attacker_img = asset_server.load("player.png");
 
+    let attacker_pos = Vec2::new(half_window_size.x - padding, 0.);
     let defender_pos = Vec2::new(-half_window_size.x + padding, 0.);
-    commands.spawn((Defender, Position(defender_pos)));
+    let attacker_facing =
+        Quat::from_rotation_z((defender_pos - attacker_pos).to_angle() - FRAC_PI_2);
+
+    commands.spawn((
+        Attacker,
+        Sprite::from_image(attacker_img.clone()),
+        Position(attacker_pos),
+        Facing(attacker_facing.clone()),
+    ));
+
+    commands.spawn((
+        Defender,
+        Sprite::from_image(attacker_img),
+        Position(defender_pos),
+        Facing(attacker_facing.inverse()),
+    ));
 }
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn((Camera2d, Transform::from_xyz(0., 0., 0.)));
 }
 
+fn sprite_movement() {
+    // https://docs.rs/bevy/0.18.1/src/move_sprite/move_sprite.rs.html#23
+    // https://bevy.org/examples/2d-rendering/rotation/
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_camera, spawn_players))
+        .add_systems(FixedUpdate, project_positions)
         .run();
 }
